f58e4a9601fd2b668b415ed67e3401a2540bb790
Emir Shahfie <emirshahfie@gmail.com>
2025-12-12 01:28:18 +0100

    WM8960 Setup complete

--- src/main.c
+++ src/main.c
@@ -1,43 +1,27 @@
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/i2c.h>
+#include <zephyr/sys/util.h>
+#include <zephyr/drivers/i2s.h>
 #include <zephyr/sys/printk.h>
+#include <wm8960.h>
 
-/*
- * Use the default I2C0 device from your ESP32-S3 board.
- * This resolves to something like "i2c@60013000".
- */
-#define I2C_DEV DT_NODELABEL(i2c0)
+#define WM8960 DT_ALIAS(audio0)
 
-void main(void)
+int main(void)
 {
-    const struct device *i2c_dev = DEVICE_DT_GET(I2C_DEV);
+    const struct i2c_dt_spec i2c_dev = I2C_DT_SPEC_GET(WM8960);
 
-    if (!device_is_ready(i2c_dev)) {
-        printk("I2C0 device is NOT ready!\n");
-        return;
+    if(!device_is_ready(i2c_dev.bus)) {
+        printk("I2C bus device not ready\n");
+        return -1;
     }
 
-    printk("I2C0 device is ready: %s\n", i2c_dev->name);
-    printk("Starting I2C scan...\n");
-
-    uint8_t dummy = 0x00;
-    uint8_t addr;
-
-    for (addr = 0x03; addr <= 0x77; addr++) {
-
-        /* Try writing a single dummy byte */
-        int ret = i2c_write(i2c_dev, &dummy, 1, addr);
-
-        if (ret == 0) {
-            printk("FOUND device at 0x%02X\n", addr);
-        } else if (ret != -EIO && ret != -ENXIO) {
-            /* Only print unexpected errors */
-            printk("Address 0x%02X returned error %d\n", addr, ret);
-        }
-
-        k_msleep(5);
+    int ret = wm8960_setup(&i2c_dev);
+    
+    if (ret) {
+        printk("WM8960 setup failed\n");
+        return ret;
     }
-
-    printk("I2C scan finished.\n");
+    return 0;
 }
